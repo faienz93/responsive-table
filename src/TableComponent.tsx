5899db2349629a2d4b02f02632674635a43ac685
faienz93 <afaienza93@gmail.com>
2025-01-22 10:25:10 +0100

    created table structure

---
+++ src/TableComponent.tsx
@@ -0,0 +1,28 @@
+
+
+import { students} from './student.tsx'
+
+export default function TableComponent() {
+    return (
+        <table>
+          <thead>
+            <tr>
+              <th>Student ID</th>
+              <th>Name</th>
+              <th>Date of Birth</th>
+              <th>Major</th>
+            </tr>
+          </thead>
+          <tbody>
+            {students.map(student => (
+              <tr key={student.id}>
+                <td>{student.id}</td>
+                <td>{student.name}</td>
+                <td>{student.dob}</td>
+                <td>{student.major}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      );
+}
---
+++ src/student.tsx
@@ -0,0 +1,12 @@
+export const students = [
+  {
+    id: 1,
+    name: "Alice Johnson",
+    dob: "2001-04-15",
+    major: "Computer Science",
+  },
+  { id: 2, name: "Bob Smith", dob: "2000-09-08", major: "Mathematics" },
+  { id: 3, name: "Carol Williams", dob: "1999-02-23", major: "Physics" },
+  { id: 4, name: "David Jones", dob: "2001-12-30", major: "Chemistry" },
+  { id: 5, name: "Eve Brown", dob: "2000-07-11", major: "Biology" },
+];
