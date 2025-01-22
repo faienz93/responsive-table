b31f37b7668a43e7fab90d27965a16f2cbbd2eff
faienz93 <afaienza93@gmail.com>
2025-01-22 11:35:45 +0100

    finished responsive table

--- src/components/TableComponent.tsx
+++ src/components/TableComponent.tsx
@@ -1,29 +1,37 @@
 
 
-import { students} from '../student.tsx'
+import { students } from '../student.tsx'
 import './TableComponent.css'
 
-export default function TableComponent({className}) {
-    return (
-        <table className={className}>
-          <thead>
-            <tr>
-              <th>Student ID</th>
-              <th>Name</th>
-              <th>Date of Birth</th>
-              <th>Major</th>
-            </tr>
-          </thead>
-          <tbody>
-            {students.map(student => (
-              <tr key={student.id}>
-                <td>{student.id}</td>
-                <td>{student.name}</td>
-                <td>{student.dob}</td>
-                <td>{student.major}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      );
+export default function TableComponent({ className }) {
+  return (
+    <table className={className}>
+      <thead>
+        <tr>
+          <th>Student ID</th>
+          <th>Name</th>
+          <th>Date of Birth</th>
+          <th>Major</th>
+        </tr>
+      </thead>
+      <tbody>
+        {students.map(student => (
+          <tr key={student.id}>
+            <td>
+              <span className="cell-header">Student ID:</span> {student.id}
+            </td>
+            <td>
+              <span className="cell-header">Name:</span> {student.name}
+            </td>
+            <td>
+              <span className="cell-header">Date of Birth:</span> {student.dob}
+            </td>
+            <td>
+              <span className="cell-header">Major:</span> {student.major}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
